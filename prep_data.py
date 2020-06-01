b459aed96d85e09ba4b1173c4c2345a77996bb53
Dennis Harrop <dh450@exeter.ac.uk>
2020-06-01 17:44:37 +0100

    formatted

--- prep_data.py
+++ prep_data.py
@@ -6,7 +6,9 @@ from tqdm import tqdm
 import os
 
 #  Save scene graph objects from the data file 'data/by-id/'
-def save_scene_graphs_as_objs(start_index=1, end_index=108079,min_rels=0,split_size=0):
+def save_scene_graphs_as_objs(
+    start_index=1, end_index=108079, min_rels=0, split_size=0
+):
     """
     Helper function to convert scene graphs from the visual genome 'data/by-id/' directory into pickle object files
 
@@ -16,19 +18,24 @@ def save_scene_graphs_as_objs(start_index=1, end_index=108079,min_rels=0,split_s
         min_rels
         split_size - number of scene graphs per object file
     """
-    split_points = [min(x,end_index) for x in range(split_size,end_index + split_size,split_size)]
+    split_points = [
+        min(x, end_index) for x in range(split_size, end_index + split_size, split_size)
+    ]
     prev = 1
     for i in split_points:
-        print(prev,i)
-        temp_scene_graphs = vg.get_scene_graphs(start_index=prev,end_index=i,min_rels=15)
+        print(prev, i)
+        temp_scene_graphs = vg.get_scene_graphs(
+            start_index=prev, end_index=i, min_rels=15
+        )
         print(len(temp_scene_graphs))
-        fh = open('data/scene_graph_objs/'+str(i)+'.obj','wb')
-        pickle.dump(temp_scene_graphs,fh)
-        temp_scene_graphs = [] 
+        fh = open("data/scene_graph_objs/" + str(i) + ".obj", "wb")
+        pickle.dump(temp_scene_graphs, fh)
+        temp_scene_graphs = []
         prev = i
 
-# Check if a scene graph contains specified objects 
-def scene_graph_contains(sg,objects):
+
+# Check if a scene graph contains specified objects
+def scene_graph_contains(sg, objects):
     """ 
     Check if a scene graph contains any objects from a specified list
 
@@ -39,14 +46,15 @@ def scene_graph_contains(sg,objects):
     Output:
         True - if the scene graph does contain any of the specified objects
         False - otherwise
-    """    
+    """
     for o in sg.objects:
         for n in o.names:
             if n in objects:
                 return True
     return False
 
-def filter_scene_graphs(path,filters):
+
+def filter_scene_graphs(path, filters):
     """ 
     Filter scene graphs based on list of object names  
     
@@ -59,13 +67,9 @@ def filter_scene_graphs(path,filters):
     """
     filtered_sgs = []
     for fn in tqdm(os.listdir(path)):
-        with open(os.path.join(path,fn),'rb') as f:
+        with open(os.path.join(path, fn), "rb") as f:
             sgs = pickle.load(f)
             for g in sgs:
-                if scene_graph_contains(g,filters):
+                if scene_graph_contains(g, filters):
                     filtered_sgs.append(g)
     return filtered_sgs
-
-
-
-
