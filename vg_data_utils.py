0ba882289468f84fd147d28fa16af2d5442eec7a
Dennis Harrop <dh450@exeter.ac.uk>
2020-06-02 15:36:11 +0100

    formatted code

--- vg_data_utils.py
+++ vg_data_utils.py
@@ -54,65 +54,65 @@ def scene_graph_contains(sg, objects):
                 return True
     return False
 
+
 def get_filtered_ids(filters):
     filters.sort()
-    image_ids = vg.get_all_image_data('data/')
+    image_ids = vg.get_all_image_data("data/")
     images = {img.id: img for img in image_ids}
     ids = list(images.keys())
     out = []
     for id in tqdm(ids):
-        data = json.load(open(f'data/by-id/{id}.json'))
-        graph = vg.parse_graph_local(data,images[id])
+        data = json.load(open(f"data/by-id/{id}.json"))
+        graph = vg.parse_graph_local(data, images[id])
         for o in graph.objects:
-            if scene_graph_contains(graph,filters):
+            if scene_graph_contains(graph, filters):
                 out.append(int(id))
                 break
     return out
 
+
 def write_ids_to_file(filename, ids):
-    with open(filename,'w') as out:
+    with open(filename, "w") as out:
         for i in ids:
             out.write(i)
-            out.write(' ')
+            out.write(" ")
+
 
 def get_scene_graphs_from_ids(ids):
-    image_ids = vg.get_all_image_data('data/')
+    image_ids = vg.get_all_image_data("data/")
     images = {img.id: img for img in image_ids}
-    out = [] 
+    out = []
     for id in tqdm(ids):
-        data = json.load(open(f'data/by-id/{id}.json'))
-        graph = vg.parse_graph_local(data,images[id])
-        out.append(graph)  
+        data = json.load(open(f"data/by-id/{id}.json"))
+        graph = vg.parse_graph_local(data, images[id])
+        out.append(graph)
     return out
+
 
 def load_ids_from_file(path):
     ids = []
-    with open(path,'r') as file:
+    with open(path, "r") as file:
         data = str(file.read())
-        ids = list(map(int, data.strip().split(' '))) 
+        ids = list(map(int, data.strip().split(" ")))
     return ids
 
 
 def get_scene_graphs_from_filters(filters):
     filters.sort()
     path = f'{"_".join(filters)}.txt'
-    filtered_ids =[]
+    filtered_ids = []
     if not os.path.isfile(path):
         print("Filters dont already exits. Filtering data")
-        filtered_ids = get_filtered_ids(filters)  
+        filtered_ids = get_filtered_ids(filters)
         print(f"Writing filters to file at '{path}')")
-        write_ids_to_file(path,filtered_ids)
+        write_ids_to_file(path, filtered_ids)
     else:
         print("Filtered id already exist loadint from file at '{path}'")
-        filtered_ids = load_ids_from_file(path) 
-   
+        filtered_ids = load_ids_from_file(path)
+
     print("Loading scene graphs from filtered ids")
     return get_scene_graphs_from_ids(filtered_ids)
-        
 
-
-filters = ['dog','cat','person']
-print(get_scene_graphs_from_filters(filters))
-
-
-
+if __name__ == "__main__":
+    filters = ["dog", "cat", "person"]
+    print(get_scene_graphs_from_filters(filters))
