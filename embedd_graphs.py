3c016b256ff8bcaba025e1928a3758f53038db1b
Dennis Harrop <dh450@exeter.ac.uk>
2020-06-05 22:32:26 +0100

    ability plot images with grpahs, also added information to G.graph

--- embedd_graphs.py
+++ embedd_graphs.py
@@ -3,16 +3,38 @@ from eden.graph import vectorize
 import networkx as nx
 from networkx.readwrite import json_graph
 import ujson as json
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import urllib
 
 if __name__ == "__main__":
     graphs = []
-    for entry in os.scandir("data/filtered/zebra"):
+    for entry in tqdm(os.scandir("data/filtered/cat-zebra")):
         if entry.name.endswith("json"):
             graphs.append(json_graph.node_link_graph(json.loads(open(entry).read())))
+        break
+    G = graphs[0]
+    label = nx.get_node_attributes(G, "label")
+    svec = nx.get_node_attributes(G, "svec")
+    e_label = nx.get_edge_attributes(G, "label")
+    e_weights = nx.get_edge_attributes(G, "weight")
 
-    print(graphs[0].edges(data=True))
-    print()
-    print(nx.is_connected(graphs[0].to_undirected()))
-    print((graphs[0].nodes(data=True)))
-    X = vectorize(graphs, complexity=2, nbits=16, discrete=False)
-    print(X)
+    pos = {n: [svec[n]["x"], svec[n]["y"]] for n in G.nodes}
+
+    f = urllib.request.urlopen(G.graph["url"])
+    a = plt.imread(f, 0)
+    plt.imshow(a, interpolation="nearest", aspect="auto", extent=[0, 100, 0, 100])
+
+    nodes = nx.draw_networkx_nodes(G, pos, node_size=50, node_color="blue",)
+    edges = nx.draw_networkx_edges(
+        G, pos, node_size=15, arrowstyle="->", arrowsize=10, edge_color="blue", width=1,
+    )
+    edge_labels = nx.draw_networkx_edge_labels(
+        G, pos, edge_labels=e_label, bbox=dict(alpha=0), font_size=8, rotate=False
+    )
+    node_labels = nx.draw_networkx_labels(G, pos, labels=label,)
+    plt.axis([0, 100, 0, 100])
+    plt.title(f"image id: {G.graph['id']}")
+    plt.tight_layout()
+    plt.show()
+
--- sg_to_nx_graph.py
+++ sg_to_nx_graph.py
@@ -15,13 +15,10 @@ def sg_to_nx(sg):
     w = sg.image.width
     h = sg.image.height
 
-    g = nx.DiGraph(w=w, h=h)
+    g = nx.DiGraph(w=w, h=h, url=sg.image.url, id=sg.image.id)
     for r in sg.relationships:
         g.add_edge(
-            r.subject.id,
-            r.object.id,
-            label=r.predicate,
-            #  weight=relative_dist_between_objects(r.subject, r.object, w, h),
+            r.subject.id, r.object.id, label=r.predicate,
         )
     for o in sg.objects:
         attributes = {d: 1 for d in o.attributes}
@@ -40,18 +37,13 @@ def solve_mst(nxg):
     temp = nx.Graph()
     attributes = nx.get_node_attributes(nxg, "svec")
     edges = list(itertools.permutations(nxg.nodes, 2))
-    print(len(edges))
-
     ttt = [e for e in edges if not e[0] == e[1]]
-    print(len(ttt))
     n_edged = []
     for e in edges:
         n, nn = e
         na = attributes[n]
         nna = attributes[nn]
-        dist = relative_dist_between_points(
-            na["x"], na["y"], nna["x"], nna["y"], nxg.graph["w"], nxg.graph["h"]
-        )
+        dist = dist_between_points(na["x"], na["y"], nna["x"], nna["y"])
         dist = int(dist)
         n_edged.append((n, nn, {"label": "near", "weight": dist}))
     temp.add_edges_from(n_edged)
@@ -63,58 +55,17 @@ def solve_mst(nxg):
         nxg.add_edge(v, u, label="near", weight=g["weight"])
 
 
-def relative_dist_between_points(x, y, x2, y2, w, h):
+def dist_between_points(x, y, x2, y2):
     return (((x2 - x) ** 2 + (y2 - y) ** 2)) ** 0.5
 
 
-from networkx.drawing.nx_pydot import write_dot
-
 if __name__ == "__main__":
-    scene_graphs = get_scene_graphs(filters=["zebra"])
-    #  scene_graphs = get_scene_graphs()
-    graphs = []
+    scene_graphs = get_scene_graphs(filters=["cat", "zebra"])
     for sg in tqdm(scene_graphs):
         id = sg.image.id
         g = sg_to_nx(sg)
         solve_mst(g)
-        graphs.append(g)
-        print(g.edges(data=True))
-        print()
+
         data = json_graph.node_link_data(g)
-        #  print(data)
-        with open(f"data/filtered/zebra/{id}.json", "w") as file:
-            file.write(json.dumps(data))
-    G = graphs[1]
-
-    draw_graph(
-        G,
-        secondary_vertex_label="svec",
-        secondary_edge_label="weight",
-        size=15,
-        vertex_size=500,
-        font_size=14,
-        vertex_border=True,
-        size_x_to_y_ratio=3,
-    )
-    #  sgs = {}
-#
-#  print(scene_graphs[0].image)
-#  for entry in os.scandir("data/filtered/zebra"):
-#  if entry.path.endswith("gefx"):
-#  with open(entry) as file:
-#  sg = nx.read_gexf(entry.path, node_type=int)
-#  sgs[entry.name.split(".")[0]] = sg
-
-#  print(solve_mst(sgs["134"]).edges(data=True))
-
-#  draw_graph(
-#  sgs[0],
-#  size=15,
-#  vertex_size=1500,
-#  font_size=14,
-#  vertex_border=True,
-#  size_x_to_y_ratio=3,
-#  )
-#  nx.write_multiline_adjlist(g, "test")
-#  test = nx.read_multiline_adjlist("test", nodetype=int, create_using=nx.DiGraph)
-#  print(list(nx.generate_multiline_adjlist(test)))
+        with open(f"data/filtered/cat-zebra/{id}.json", "w") as file:
+            zebra.write(json.dumps(data))
